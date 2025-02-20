f09364316bf281a7fbdb3bb4e8cc03e7731c5eca
antony-lucide <113905621+antony-lucide@users.noreply.github.com>
2025-02-20 14:27:37 +0100

    J'ai enfin réussi

---
+++ monPremierFichier.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, C++!" << std::endl;
+    int num = 5;
+    int num2 = 10;
+    int sum = num + num2;
+    std::cout << "La somme de " << num << " et " << num2 << " est " << sum << std::endl;
+    return 0;
+}
